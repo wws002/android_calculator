3d95429c9459ac863eb9171e248763a2facbd50f
Will  Smith <wws002@email.uark.edu>
2019-09-12 12:58:16 -0500

    we have basic addition, subtraction, multiplication, and division

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -17,43 +17,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onButtonClicked(View v){
+    public void onButtonClicked(View v) {
         TextView textBar = findViewById(R.id.textBar);
 
-        if(v.getId() == R.id._0)
+        if (v.getId() == R.id._0)
             textBar.append("0");
-        if(v.getId() == R.id._1)
+        if (v.getId() == R.id._1)
             textBar.append("1");
-        if(v.getId() == R.id._2)
+        if (v.getId() == R.id._2)
             textBar.append("2");
-        if(v.getId() == R.id._3)
+        if (v.getId() == R.id._3)
             textBar.append("3");
-        if(v.getId() == R.id._4)
+        if (v.getId() == R.id._4)
             textBar.append("4");
-        if(v.getId() == R.id._5)
+        if (v.getId() == R.id._5)
             textBar.append("5");
-        if(v.getId() == R.id._6)
+        if (v.getId() == R.id._6)
             textBar.append("6");
-        if(v.getId() == R.id._7)
+        if (v.getId() == R.id._7)
             textBar.append("7");
-        if(v.getId() == R.id._8)
+        if (v.getId() == R.id._8)
             textBar.append("8");
-        if(v.getId() == R.id._9)
+        if (v.getId() == R.id._9)
             textBar.append("9");
 
-        if(v.getId() == R.id.plus)
+        if (v.getId() == R.id.plus)
             textBar.append("+");
-        if(v.getId() == R.id.minus)
+        if (v.getId() == R.id.minus)
             textBar.append("-");
-        if(v.getId() == R.id.times)
+        if (v.getId() == R.id.times)
             textBar.append("*");
-        if(v.getId() == R.id.divided_by)
+        if (v.getId() == R.id.divided_by)
             textBar.append("/");
-        if(v.getId() == R.id.point)
+        if (v.getId() == R.id.point)
             textBar.append(".");
     }
 
-    public void onClearClicked(View v){
+    public void onClearClicked(View v) {
         TextView textBar = findViewById(R.id.textBar);
         textBar.setText("");
     }
@@ -62,10 +62,36 @@ public class MainActivity extends AppCompatActivity {
         TextView textBar = findViewById(R.id.textBar);
         String currentText = textBar.getText().toString();
 
-        if(currentText.length() > 0)
-        {
+        if (currentText.length() > 0) {
             String newText = currentText.substring(0, currentText.length() - 1);
             textBar.setText(newText);
+        }
+    }
+
+    public void onEqualsClicked(View v) {
+        TextView textBar = findViewById(R.id.textBar);
+        String currentText = textBar.getText().toString();
+
+        if (currentText.length() > 2) {
+            String num1 = currentText.substring(0, 1);
+            String operand = currentText.substring(1, 2);
+            String num2 = currentText.substring(2, 3);
+
+            int int1 = Integer.parseInt(num1);
+            int int2 = Integer.parseInt(num2);
+            int result = 0;
+
+            if (operand.equals("+"))
+                result = int1 + int2;
+            if(operand.equals("-"))
+                result = int1 - int2;
+            if(operand.equals("*"))
+                result = int1 * int2;
+            if(operand.equals("/"))
+                result = int1 / int2;
+
+            String textResult = Integer.toString(result);
+            textBar.setText(textResult);
         }
     }
 }
